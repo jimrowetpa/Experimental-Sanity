f2ea2ad5f101bea87d495827031c1b6317e28656
RC Maples <rc.maples@sanity.io>
2025-02-14 16:42:06 -0500

    removed route handler functions in favor of intent link components

--- components/TestComponent.tsx
+++ components/TestComponent.tsx
@@ -1,48 +1,32 @@
+// @ts-nocheck
 import React from 'react'
-import {useRouter} from 'sanity/router'
+import {IntentLink} from 'sanity/router'
+
 import {Button} from '@sanity/ui'
 
 const TestComponent = () => {
-  const router = useRouter()
-
-  const handleCreatePage = () => {
-    router.navigateIntent('create', {
-      type: 'page',
-      template: "page-template",
-      params: {
-        title: "Test Page",
-        test: "Page params from navigate Intent"
-      }
-    })
-  }
-
-  const handleCreatePageWithTemplate2 = () => {
-    router.navigateIntent('create', {
-      type: 'page',
-      template: "page-template-2",
-      params: {
-        title: "Test Page 2",
-        test: "Page params from navigate Intent for template 2"
-      }
-    })
-  }
-
   return (
     <div>
       <h1>Hello World from Test Component!</h1>
-      <Button 
-        tone="primary" 
-        onClick={handleCreatePage}
+      <IntentLink
+        intent="create"
+        params={[
+          {type: 'page', template: 'page-template'},
+          {title: 'Test Page', test: 'Page params from navigate Intent'},
+        ]}
       >
-        Create Page with template
-      </Button>
+        <Button tone="primary">Create Page with template</Button>
+      </IntentLink>
 
-      <Button 
-        tone="primary" 
-        onClick={handleCreatePageWithTemplate2}
+      <IntentLink
+        intent="create"
+        params={[
+          {type: 'page', template: 'page-template'},
+          {title: 'Test Page 2', test: 'Page params from navigate Intent for template 2'},
+        ]}
       >
-        Create Page with template 2
-      </Button>
+        <Button tone="primary">Create Page with template 2</Button>
+      </IntentLink>
     </div>
   )
 }
